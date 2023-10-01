f4f5ec2c47a91c5606839f483e0173caf424a494
Th3GaM3RCaT <rodrigochanqueyo@gmail.com>
2023-10-01 15:02:45 -0300

    visualizar datos de usuario
    por ahora solo está lo del nombre del usuario

--- app/src/main/java/com/thecurseds/gamequest/VerPerfil.java
+++ app/src/main/java/com/thecurseds/gamequest/VerPerfil.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class VerPerfil extends AppCompatActivity {
+
+    TextView nombreUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_perfil);
+        nombreUsuario = findViewById(R.id.txt_nombreUsuario);
+        DatosUsuario();
     }
     public void IrInicio (View v){
         finish();
+    }
+
+    public void DatosUsuario (){
+        nombreUsuario.setText("Rodrigo");
+
     }
 }
