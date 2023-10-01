08391089fa437f34d398bbd465eecfba3ce7bca8
SebastianIHS <134305630+SebastianIHS@users.noreply.github.com>
2023-09-30 21:33:18 -0300

    WA QUE SE ACABE MI SUFRIMIENTO PORFA
    HAY MUCHAS WEAS QUE NI VOY A OCUPAR, ESTOY TROLEANDO

--- app/src/main/java/com/thecurseds/gamequest/MainActivity.java
+++ app/src/main/java/com/thecurseds/gamequest/MainActivity.java
@@ -1,25 +1,22 @@
 package com.thecurseds.gamequest;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+
+    private Button registroButton;
+    private Button logearseButton;
+
 
     public void IrEditarMiPerfil (View v){
         Intent intento = new Intent(this, EditarMiPerfil.class);
         startActivity(intento);
-
-
     }
+
 
     ///lo arregle ctm
 
