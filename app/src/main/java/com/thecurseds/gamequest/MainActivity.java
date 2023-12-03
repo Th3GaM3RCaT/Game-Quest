7131a00dfd162f8320d2bfab2607ad293c413116
Th3GaM3RCaT <rodrigochanqueyo@gmail.com>
2023-12-03 18:26:32 -0300

    si

--- app/src/main/java/com/thecurseds/gamequest/MainActivity.java
+++ app/src/main/java/com/thecurseds/gamequest/MainActivity.java
@@ -5,18 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonCompra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonCompra = findViewById(R.id.btn_compra);
     }
 
     public void IrLogin (View v){
         Intent intent = new Intent(this, MainLogin.class);
         startActivity(intent);
     }
+
+
+
 
 }
